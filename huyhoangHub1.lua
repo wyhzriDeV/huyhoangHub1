74710347de87d2b00f83853849031b8759732278
Huy Hoang Dz <phannguyenhuyhoang2024py@gmail.com>
2025-07-11 13:27:18 +0700

    huyhoangHub1.lua

--- huyhoangHub1.lua
+++ huyhoangHub1.lua
@@ -5,28 +5,29 @@ local LocalPlayer = Players.LocalPlayer
 local screenGui = Instance.new("ScreenGui")
 screenGui.Name = "HuyHoangJumpscareGUI"
 screenGui.ResetOnSpawn = false
+screenGui.IgnoreGuiInset = true  
 screenGui.Parent = LocalPlayer:WaitForChild("PlayerGui")
 
 local imageLabel = Instance.new("ImageLabel")
 imageLabel.Size = UDim2.new(1, 0, 1, 0)
 imageLabel.Position = UDim2.new(0, 0, 0, 0)
-imageLabel.Image = "rbxassetid://9103670847"
+imageLabel.Image = "rbxassetid://9103670847" 
 imageLabel.BackgroundTransparency = 1
 imageLabel.ImageTransparency = 1
+imageLabel.ZIndex = 999999  
 imageLabel.Parent = screenGui
 
 local sound = Instance.new("Sound")
 sound.SoundId = "rbxassetid://91749485638258"
-sound.Volume = 190
+sound.Volume = 10
 sound.Looped = false
 sound.Parent = LocalPlayer:WaitForChild("PlayerGui")
 
+sound:Play()
+
 for i = 1, 10 do
 	imageLabel.ImageTransparency = imageLabel.ImageTransparency - 0.1
-	wait(0.05)
-end
-
-sound:Play()
+	wait(0.02)
 
 wait(2.5)
 LocalPlayer:Kick("666ERROR-KICK-BANNED")
