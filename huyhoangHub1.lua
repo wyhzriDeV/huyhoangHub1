11381ac9c41a0fdf9c5a13666c6fb123d028db18
Huy Hoang Dz <phannguyenhuyhoang2024py@gmail.com>
2025-07-11 12:18:06 +0700

    Add files via upload

---
+++ huyhoangHub1.lua
@@ -0,0 +1,37 @@
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+local LocalPlayer = Players.LocalPlayer
+
+local screenGui = Instance.new("ScreenGui")
+screenGui.Name = "HuyHoangJumpscareGUI"
+screenGui.ResetOnSpawn = false
+screenGui.Parent = LocalPlayer:WaitForChild("PlayerGui")
+
+local imageLabel = Instance.new("ImageLabel")
+imageLabel.Size = UDim2.new(1, 0, 1, 0)
+imageLabel.Position = UDim2.new(0, 0, 0, 0)
+imageLabel.Image = "rbxassetid://84889810608201"
+imageLabel.BackgroundTransparency = 1
+imageLabel.ImageTransparency = 1
+imageLabel.Parent = screenGui
+
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://91749485638258"
+sound.Volume = 10
+sound.Looped = false
+sound.Parent = LocalPlayer:WaitForChild("PlayerGui")
+
+for i = 1, 10 do
+	imageLabel.ImageTransparency = imageLabel.ImageTransparency - 0.1
+	wait(0.05)
+end
+
+sound:Play()
+
+wait(2.5)
+LocalPlayer:Kick("MÀY ĐÃ BỊ TROLL HAHAHA🤣")
+
+while true do
+	wait(2)
+	TeleportService:Teleport(game.PlaceId, LocalPlayer)
+end
